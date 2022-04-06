f6764a3bc22e25d5e58a21653e4cd21350fcf63d
이선민 <smlee@pangpany.co.kr>
2022-04-07 05:51:00 +0900

    드롭다운 토글, 외부클릭시 닫힘

--- project/resources/js/style.js
+++ project/resources/js/style.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 	//로그아웃 드롭다운
-	$('.dropdown-toggle').click(function () {
-		$(this).siblings('.dropdown-menu').toggle();
+	var target = $(".dropdown-menu");
+	$(".dropdown-toggle").click(function(event){ 
+		event.stopPropagation(); 
+		target.toggle();
+	}); 
+	$(document).click(function(){ //외부클릭시 닫힘
+		target.hide(); 
 	});
+	
+	
+	
 });
 
 
